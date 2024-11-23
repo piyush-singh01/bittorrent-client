b7dba5408186a69219fbdbb2f84c0b16b7f88f8c
piyushsingh01 <piyushsingh.cs@gmail.com>
2024-11-23 13:48:09 +0530

    added additional methods and fields

--- session.go
+++ session.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"encoding/hex"
 	"fmt"
 	"log"
 	"net"
 	"strconv"
+	"sync"
 	"time"
 )
 
@@ -17,19 +17,29 @@ type Configurable struct {
 	initialTcpConnectionTimeout time.Duration
 	listenerPort                uint16
 	trackerPollInterval         time.Duration
+	keepAliveTickInterval       time.Duration
+	rateTrackerTickerInterval   time.Duration
 }
 
 type TorrentSession struct {
-	torrent        *Torrent
-	localPeerId    [20]byte
-	configurable   *Configurable
-	peerConnection map[string]*PeerConnection
-	bitfield       *Bitset
-	//connectionChannel chan *PeerConnection // todo: discard this and use the two read and write channels as below
-	readChannel       chan *PeerConnection
-	writeChannel      chan *PeerConnection
-	listener          *Listener
-	trackerPollTicker *time.Ticker
+	torrent            *Torrent
+	muInterestedPeers  sync.RWMutex
+	muPeerConnection   sync.RWMutex
+	localPeerId        [20]byte
+	configurable       *Configurable
+	peerConnection     map[string]*PeerConnection
+	rateTracker        *RateTracker
+	bitfield           *Bitset
+	quitChannel        chan *PeerConnection
+	listener           *Listener
+	trackerPollTicker  *time.Ticker
+	keepAliveTicker    *time.Ticker
+	interestedPeers    map[string]*PeerConnection
+	peerRequests       map[string]*BlockRequest
+	ourRequests        map[string]*BlockRequest
+	peerMessageChannel chan *PeerConnectionMessage
+	maxUploadSlots     int // max number of unchoked peers at a time
+	rateTrackerTicker  *time.Ticker
 }
 
 func NewTorrentSession(torrent *Torrent, localPeerId [20]byte) (*TorrentSession, error) {
@@ -39,31 +49,63 @@ func NewTorrentSession(torrent *Torrent, localPeerId [20]byte) (*TorrentSession,
 		initialTcpConnectionTimeout: time.Second * 5,
 		trackerResponseMinPeers:     4,
 		listenerPort:                8888,
-		trackerPollInterval:         time.Second * 5,
+		keepAliveTickInterval:       time.Second * 120,
+		rateTrackerTickerInterval:   time.Second,
 	}
 	return &TorrentSession{
-		torrent:        torrent,
-		localPeerId:    localPeerId,
-		configurable:   configurable,
-		peerConnection: make(map[string]*PeerConnection),
-		bitfield:       NewBitset(torrent.Info.NumPieces),
-		//connectionChannel: make(chan *PeerConnection, 50),
-		readChannel:  make(chan *PeerConnection, 50),
-		writeChannel: make(chan *PeerConnection, 50),
-		listener:     nil,
+		torrent:            torrent,
+		localPeerId:        localPeerId,
+		configurable:       configurable,
+		peerConnection:     make(map[string]*PeerConnection),
+		rateTracker:        NewRateTracker(),
+		bitfield:           NewBitset(torrent.Info.NumPieces),
+		quitChannel:        make(chan *PeerConnection, 10),
+		listener:           nil,
+		peerRequests:       make(map[string]*BlockRequest),
+		ourRequests:        make(map[string]*BlockRequest),
+		peerMessageChannel: make(chan *PeerConnectionMessage),
+		maxUploadSlots:     5,
 	}, nil
 }
 
-func (ts *TorrentSession) StartTrackerPollTickerWithInterval(trackerPollInterval time.Duration) {
+func (ts *TorrentSession) SetTrackerPolling(trackerPollInterval time.Duration) {
 	if ts.trackerPollTicker != nil {
 		ts.trackerPollTicker.Stop()
 	}
 
+	ts.configurable.trackerPollInterval = trackerPollInterval
 	ts.trackerPollTicker = time.NewTicker(trackerPollInterval)
 }
 
-func (ts *TorrentSession) StopTrackerPollTicker() {
+func (ts *TorrentSession) StopTrackerPolling() {
+	if ts.trackerPollTicker == nil {
+		log.Printf("tracker polling is already stopped")
+	}
 	ts.trackerPollTicker.Stop()
+}
+
+func (ts *TorrentSession) SetRateTrackerTicker() {
+	if ts.rateTrackerTicker != nil {
+		ts.rateTrackerTicker.Stop()
+	}
+
+	ts.rateTrackerTicker = time.NewTicker(ts.configurable.rateTrackerTickerInterval)
+}
+
+func (ts *TorrentSession) StopRateTrackerTicker() {
+	if ts.rateTrackerTicker == nil {
+		log.Printf("rate tracker ticker is already stopped")
+		return
+	}
+	ts.rateTrackerTicker.Stop()
+}
+
+// StartQuitter Meant to run as a goroutine
+func (ts *TorrentSession) StartQuitter() {
+	for {
+		connection := <-ts.quitChannel
+		connection.CloseConnection(ts)
+	}
 }
 
 func (ts *TorrentSession) MountListenerOnDefaultPort() error {
@@ -105,10 +147,10 @@ func (ts *TorrentSession) StartListening() error {
 
 		receivedHandshake, err := HandleHandshake(conn, ts)
 		if err != nil {
-			log.Printf("can not perform handshake with incoming peer")
+			log.Printf("can not perform handshake with incoming peer: %v", err)
 			continue
 		}
-
+		log.Printf("handshake successfull with peer %s", receivedHandshake.PeerId)
 		peer := Peer{
 			PeerId: receivedHandshake.PeerId,
 			IP:     ip,
@@ -116,7 +158,8 @@ func (ts *TorrentSession) StartListening() error {
 			Port:   uint16(port),
 		}
 
-		ts.readChannel <- NewPeerConnection(peer, conn)
+		NewPeerConnectionWithReaderAndWriter(peer, conn, ts)
+		log.Printf("peer connection created with reader and writer goroutines, with peer %s", receivedHandshake.PeerId)
 	}
 }
 
@@ -129,26 +172,117 @@ func (ts *TorrentSession) CloseListener() {
 	log.Printf("listener closed")
 }
 
+func (ts *TorrentSession) AddPeerToInterestedList(peer *PeerConnection) error {
+	ts.muInterestedPeers.Lock()
+	defer ts.muInterestedPeers.Unlock()
+
+	if _, ok := ts.interestedPeers[peer.peerIdStr]; ok {
+		return fmt.Errorf("peer already in interseted peer list")
+	}
+	ts.interestedPeers[peer.peerIdStr] = peer
+	return nil
+}
+
+func (ts *TorrentSession) RemovePeerFromInterestedList(peer *PeerConnection) error {
+	ts.muInterestedPeers.Lock()
+	defer ts.muInterestedPeers.Unlock()
+
+	if _, ok := ts.interestedPeers[peer.peerIdStr]; !ok {
+		return fmt.Errorf("peer not in interseted peer list")
+	}
+	delete(ts.interestedPeers, peer.peerIdStr)
+	return nil
+}
+
+func (ts *TorrentSession) CheckPeerInInterestedList(peer *PeerConnection) bool {
+	ts.muInterestedPeers.RLock()
+	defer ts.muInterestedPeers.RUnlock()
+
+	_, ok := ts.interestedPeers[peer.peerIdStr]
+	return ok
+}
+
 func (ts *TorrentSession) AddPeerToActiveList(peer *PeerConnection) error {
-	peerIdStr := hex.EncodeToString(peer.peerId[:])
-	if _, ok := ts.peerConnection[peerIdStr]; ok {
+	ts.muPeerConnection.Lock()
+	defer ts.muPeerConnection.Unlock()
+
+	peer.isActive = true
+	if _, ok := ts.peerConnection[peer.peerIdStr]; ok {
 		return fmt.Errorf("peer already in active peer list")
 	}
-	ts.peerConnection[peerIdStr] = peer
+	ts.peerConnection[peer.peerIdStr] = peer
 	return nil
 }
 
 func (ts *TorrentSession) RemovePeerFromActiveList(peer *PeerConnection) error {
+	ts.muPeerConnection.Lock()
+	defer ts.muPeerConnection.Unlock()
+
+	peer.isActive = false
 	if _, ok := ts.peerConnection[peer.peerIdStr]; !ok {
 		return fmt.Errorf("error removing: peer not present in active peer list")
 	}
 	delete(ts.peerConnection, peer.peerIdStr)
+	ts.rateTracker.RemoveConnection(peer.peerIdStr)
 	return nil
 }
 
+// CheckPeerInActiveList redundant, not needed
+func (ts *TorrentSession) CheckPeerInActiveList(peer *PeerConnection) bool {
+	ts.muPeerConnection.RLock()
+	defer ts.muPeerConnection.RUnlock()
+
+	return peer.isActive
+}
+
+//func (ts *TorrentSession) AddPeerRequest(peer *PeerConnection, b *BlockRequest) error {
+//	ts.mu.Lock()
+//	defer ts.mu.Unlock()
+//
+//	if _, ok := ts.peerRequests[peer.peerIdStr]; ok {
+//		return fmt.Errorf("block already requested by this peer %s", peer.peerIdStr)
+//	}
+//	ts.peerRequests[peer.peerIdStr] = b
+//	return nil
+//}
+//
+//func (ts *TorrentSession) AddOurRequest(peer *PeerConnection, b *BlockRequest) error {
+//	ts.mu.Lock()
+//	defer ts.mu.Unlock()
+//
+//	if _, ok := ts.ourRequests[peer.peerIdStr]; ok {
+//		return fmt.Errorf("block already requested to this peer %s", peer.peerIdStr)
+//	}
+//	ts.ourRequests[peer.peerIdStr] = b
+//	return nil
+//}
+//
+//// RemovePeerRequest if request by the peer has been fulfilled
+//func (ts *TorrentSession) RemovePeerRequest(peer *PeerConnection, b *BlockRequest) error {
+//	ts.mu.Lock()
+//	defer ts.mu.Unlock()
+//
+//	if _, ok := ts.peerRequests[peer.peerIdStr]; !ok {
+//		return fmt.Errorf("this request by the peer %s doesn't exist", peer.peerIdStr)
+//	}
+//	delete(ts.peerRequests, peer.peerIdStr)
+//	return nil
+//}
+//
+//func (ts *TorrentSession) RemoveOurRequest(peer *PeerConnection, b *BlockRequest) error {
+//	ts.mu.Lock()
+//	defer ts.mu.Unlock()
+//
+//	if _, ok := ts.ourRequests[peer.peerIdStr]; !ok {
+//		return fmt.Errorf("this request to the peer %s doesn't exist", peer.peerIdStr)
+//	}
+//	delete(ts.ourRequests, peer.peerIdStr)
+//	return nil
+//}
+
 func (ts *TorrentSession) SendKeepAlive(peer *PeerConnection) (n int, err error) {
 	message := NewKeepAliveMessage()
-	n, err = peer.tcpConn.Write(message.Serialize())
+	n, err = peer.WriteMessage(message, ts)
 	if err != nil {
 		return 0, fmt.Errorf("error sending 'keep-alive' message to peer %s: ", peer.peerIdStr)
 	}
@@ -157,7 +291,7 @@ func (ts *TorrentSession) SendKeepAlive(peer *PeerConnection) (n int, err error)
 
 func (ts *TorrentSession) SendChoke(peer *PeerConnection) (n int, err error) {
 	message := NewChokeMessage()
-	n, err = peer.tcpConn.Write(message.Serialize())
+	n, err = peer.WriteMessage(message, ts)
 	if err != nil {
 		return 0, fmt.Errorf("error sending 'choke' message to peer %s: ", peer.peerIdStr)
 	}
@@ -166,7 +300,7 @@ func (ts *TorrentSession) SendChoke(peer *PeerConnection) (n int, err error) {
 
 func (ts *TorrentSession) SendUnchoke(peer *PeerConnection) (n int, err error) {
 	message := NewUnchokeMessage()
-	n, err = peer.tcpConn.Write(message.Serialize())
+	n, err = peer.WriteMessage(message, ts)
 	if err != nil {
 		return 0, fmt.Errorf("error sending 'unchoke' message to peer %s: ", peer.peerIdStr)
 	}
@@ -175,7 +309,7 @@ func (ts *TorrentSession) SendUnchoke(peer *PeerConnection) (n int, err error) {
 
 func (ts *TorrentSession) SendInterested(peer *PeerConnection) (n int, err error) {
 	message := NewInterestedMessage()
-	n, err = peer.tcpConn.Write(message.Serialize())
+	n, err = peer.WriteMessage(message, ts)
 	if err != nil {
 		return 0, fmt.Errorf("error sending 'interested' message to peer %s: ", peer.peerIdStr)
 	}
@@ -184,7 +318,7 @@ func (ts *TorrentSession) SendInterested(peer *PeerConnection) (n int, err error
 
 func (ts *TorrentSession) SendNotInterested(peer *PeerConnection) (n int, err error) {
 	message := NewNotInterestedMessage()
-	n, err = peer.tcpConn.Write(message.Serialize())
+	n, err = peer.WriteMessage(message, ts)
 	if err != nil {
 		return 0, fmt.Errorf("error sending 'not-interested' message to peer %s: ", peer.peerIdStr)
 	}
@@ -193,7 +327,7 @@ func (ts *TorrentSession) SendNotInterested(peer *PeerConnection) (n int, err er
 
 func (ts *TorrentSession) SendHave(pieceIndex uint32, peer *PeerConnection) (n int, err error) {
 	message := NewHaveMessage(pieceIndex)
-	n, err = peer.tcpConn.Write(message.Serialize())
+	n, err = peer.WriteMessage(message, ts)
 	if err != nil {
 		return 0, fmt.Errorf("error sending 'have' message to peer %s: ", peer.peerIdStr)
 	}
@@ -202,7 +336,7 @@ func (ts *TorrentSession) SendHave(pieceIndex uint32, peer *PeerConnection) (n i
 
 func (ts *TorrentSession) SendBitfield(peer *PeerConnection) (n int, err error) {
 	message := NewBitfieldMessage(ts.bitfield)
-	n, err = peer.tcpConn.Write(message.Serialize())
+	n, err = peer.WriteMessage(message, ts)
 	if err != nil {
 		return 0, fmt.Errorf("error sending 'bitfield' message to peer %s: ", peer.peerIdStr)
 	}
@@ -211,7 +345,7 @@ func (ts *TorrentSession) SendBitfield(peer *PeerConnection) (n int, err error) 
 
 func (ts *TorrentSession) SendRequest(index uint32, begin uint32, length uint32, peer *PeerConnection) (n int, err error) {
 	message := NewRequestMessage(index, begin, length)
-	n, err = peer.tcpConn.Write(message.Serialize())
+	n, err = peer.WriteMessage(message, ts)
 	if err != nil {
 		return 0, fmt.Errorf("error sending 'request' message to peer %s: ", peer.peerIdStr)
 	}
@@ -220,7 +354,7 @@ func (ts *TorrentSession) SendRequest(index uint32, begin uint32, length uint32,
 
 func (ts *TorrentSession) SendPiece(index uint32, begin uint32, block []byte, peer *PeerConnection) (n int, err error) {
 	message := NewPieceMessage(index, begin, block)
-	n, err = peer.tcpConn.Write(message.Serialize())
+	n, err = peer.WriteMessage(message, ts)
 	if err != nil {
 		return 0, fmt.Errorf("error sending 'piece' message to peer %s: ", peer.peerIdStr)
 	}
@@ -229,7 +363,7 @@ func (ts *TorrentSession) SendPiece(index uint32, begin uint32, block []byte, pe
 
 func (ts *TorrentSession) SendCancel(index uint32, begin uint32, length uint32, peer *PeerConnection) (n int, err error) {
 	message := NewCancelMessage(index, begin, length)
-	n, err = peer.tcpConn.Write(message.Serialize())
+	n, err = peer.WriteMessage(message, ts)
 	if err != nil {
 		return 0, fmt.Errorf("error sending 'cancel' message to peer %s: ", peer.peerIdStr)
 	}
