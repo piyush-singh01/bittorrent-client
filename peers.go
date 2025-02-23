e227336dae3c9e004d1f843d84db6034f9aa1f07
Piyush Singh <piyush_2001cs51@iitp.ac.in>
2025-02-24 00:17:52 +0530

    moved generate local peer id to utils

--- peers.go
+++ peers.go
@@ -1,35 +1,16 @@
 package main
 
 import (
-	"crypto/rand"
-	"encoding/hex"
 	"fmt"
 	"net"
 )
-
-// generateLocalPeerId generates a Peer ID for the client.
-func generateLocalPeerId() ([20]byte, error) {
-	var localPeerId [20]byte
-
-	prefix := "-PTC001-"
-	copy(localPeerId[:], prefix)
-
-	randomBytes := make([]byte, 13)
-	_, err := rand.Read(randomBytes)
-	if err != nil {
-		return localPeerId, fmt.Errorf("failed to generate random bytes: %v", err)
-	}
-	copy(localPeerId[7:], randomBytes)
-
-	return localPeerId, nil
-}
 
 type IPType int
 
 const (
 	IPv4 IPType = iota
 	IPv6
-	Invalid
+	InvalidIpType
 )
 
 func GetIPType(ip net.IP) IPType {
@@ -38,7 +19,7 @@ func GetIPType(ip net.IP) IPType {
 	} else if ip.To16() != nil {
 		return IPv6
 	}
-	return Invalid
+	return InvalidIpType
 }
 
 type Peer struct {
@@ -55,28 +36,4 @@ func (p Peer) String() string {
 		p.Port,
 		p.PeerId,
 	)
-}
-
-type PeerState struct {
-	conn           net.Conn
-	peerId         [20]byte
-	peerIdStr      string
-	piecesBitfield *Bitset
-	amChoking      bool
-	amInterested   bool
-	peerChoking    bool
-	peerInterested bool
-}
-
-func NewPeer(peer Peer, conn net.Conn) *PeerState {
-	return &PeerState{
-		conn:           conn,
-		peerId:         peer.PeerId,
-		peerIdStr:      hex.EncodeToString(peer.PeerId[:]),
-		piecesBitfield: nil,
-		amChoking:      true,
-		amInterested:   false,
-		peerChoking:    true,
-		peerInterested: false,
-	}
 }
