85b674ec51a00fb138ec88789f2bc117b2fa0a46
Piyush Singh <piyush_2001cs51@iitp.ac.in>
2025-03-15 00:58:34 +0530

    fix in find number of blocks in last piece method

--- file-system-utils.go
+++ file-system-utils.go
@@ -9,7 +9,7 @@ import (
 func findNumBlocksLastPiece(info *InfoDict, numBlocksPerPiece int64) int64 {
 	expectedTotalLength := int64(info.NumPieces) * info.PieceLength
 	sizeOfLastPiece := expectedTotalLength - info.Length // this is the size of last piece
-	return ceilDiv(sizeOfLastPiece, numBlocksPerPiece)
+	return ceilDiv(sizeOfLastPiece, BlockSize)
 }
 
 func populatePiecesSlice(torrent *Torrent, numBlocksPerPiece int64) []*TorrentPiece {
